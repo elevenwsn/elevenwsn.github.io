f7fb0b07f371c1906347548f4408ef012e7a16b0
elevenwsn <kevintyk528@outlook.com>
2025-08-03 16:58:41 +0800

    更新 main.cpp

--- xtc/main.cpp
+++ xtc/main.cpp
@@ -2,23 +2,25 @@
 #include <fstream>
 using namespace std;
 
-int main() {
-    // 打开hosts文件(需要管理员权限)
+int main()
+{
+    //打开hosts文件，需要管理员权限
     ofstream hostsFile;
     hostsFile.open("C:\\Windows\\System32\\drivers\\etc\\hosts", ios::app);
     
-    if (!hostsFile.is_open()) {
+    if (!hostsFile.is_open())
+    {
         cout << "无法打开hosts文件，请以管理员身份运行程序" << endl;
         return 1;
     }
     
-    // 要添加的内容
+    //添加的内容
     string newEntry = "\n127.0.0.1 xxx.com";
     
-    // 写入内容
+    //写入
     hostsFile << newEntry;
     
-    // 关闭文件
+    //关闭
     hostsFile.close();
     
     cout << "已成功添加内容到hosts文件" << endl;
